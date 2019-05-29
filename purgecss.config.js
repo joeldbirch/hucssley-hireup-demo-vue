691e7b7202abebc17da5c9be37cb50a35ee0e1da
Joel Birch <joeldbirch@gmail.com>
2019-05-29 19:12:21 +1000

    Add test case in './debug' folder, point purgecss.config.js at it. Add task to run purgecss on it.

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,14 +1,32 @@
 module.exports = {
-  content: ['dist/index.html', 'dist/js/*.js'],
-  css: ['dist/css/*.css'],
+  content: ['debug/example.html'],
+  css: ['debug/purge-please.css'],
   extractors: [
     {
       extractor: class {
         static extract(content) {
-          return content.match(/(?:[A-Za-z0-9]|-|_|:|<|>|@)+/g) || [];
+          const theMatches = content.match(/(?:[A-Za-z0-9]|-|_|:|<|>|@)+/g) || [];
+          console.log({ content, theMatches });
+          return theMatches;
         }
       },
-      extensions: ['html', 'js'],
+      extensions: ['html'],
     },
   ],
 };
+
+/* RESULT:
+{
+  content: '<div class=":hover--opacity:40">this is a test</div>',
+  theMatches: [
+      '<div',
+      'class',
+      ':hover--opacity:40',
+      '>this',
+      'is',
+      'a',
+      'test<',
+      'div>'
+  ]
+}
+*/
